fa626abcd248f781174f4ed1204aa47d60f353c3
Sydney Smith <sydneydeidra.s@gmail.com>
2026-04-19 02:39:25 -0500

    lighting and background change

--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -6,36 +6,44 @@ import CameraRouteController from '../components/Route'
 import Hand from '../components/Hand'
 import Navbar from '../components/Navbar'
 import Navbar2 from '../components/Navbar2'
+import { Environment, ContactShadows } from '@react-three/drei' // Add these
 
 function Application() {
   //const [isPlaying, setIsPlaying] = useState(true)
   return (
-    <div style={{ width: '100vw', height: '100vh', background: '#050505' }}>
+    <div style={{ width: '100vw', height: '100vh', background: '#e1ebf0' }}>
       <Navbar />
       <Navbar2 />
       <Canvas
         camera={{ position: [0, 50, 100], fov: 45 }}
         shadows
-        gl={{ antialias: true }}
+        gl={{ 
+          antialias: true,
+          stencil: false,
+          depth: true
+        }}
       >
-        <ambientLight intensity={0.5} />
-        <directionalLight 
-          position={[10, 20, 10]} 
-          intensity={1.5} 
-          castShadow 
-          shadow-mapSize={[2048, 2048]}
-        />
+        {/* 1. Replace manual lights with an Environment map for realistic interior reflections */}
+        <Environment preset="city" /> 
 
         <Suspense fallback={null}>
           <CampusModel />
           <Hand />
+          
+          {/* 2. Soft floor shadows to ground the building */}
+          <ContactShadows 
+            position={[0, -0.01, 0]} 
+            opacity={0.4} 
+            scale={200} 
+            blur={2} 
+            far={50} 
+          />
         </Suspense>
 
         <BoundedOrbits />
-        <CameraRouteController
+         <CameraRouteController
           //onRouteComplete={() => setIsPlaying(false)}
         />
-
       </Canvas>
     </div>
   )
